2df203d9d826be59c18d6b197a6ecf4afb258fe3
Abhipatel2578 <159416605+Abhipatel2578@users.noreply.github.com>
2026-05-31 15:16:53 +0530

    Refactor Jenkins pipeline stages and update repository URL

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -1,47 +1,43 @@
-pipeline{
-    agent any 
-    stages{
-        stage('Code-pull'){
-            steps{
-                git branch: 'main', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
+pipeline {
+    agent any
+
+    stages {
+        stage('pull') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Abhipatel2578/flight-reservation-app11.git'
             }
         }
-        stage('Code-build'){
-            steps{
+        stage('build') {
+            steps {
                 sh '''
                 cd FlightReservationApplication
-                mvn clean package
-                '''
+                mvn clean package'''
             }
         }
-        stage('QA-TEST'){
-            steps{
-                withSonarQubeEnv(installationName:'sonar', credentialsId: 'Sonar-token') {
-                    sh '''
-                        cd FlightReservationApplication
-                        mvn sonar:sonar -Dsonar.projectKey=flight-reservation
-                    '''
-                }
+        stage('test') {
+            steps {
+                sh '''
+                 cd FlightReservationApplication
+                mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
+                -Dsonar.projectKey=Flight \
+                -Dsonar.projectName='Flight' \
+                -Dsonar.host.url=http://51.44.178.20:9000 \
+                -Dsonar.token=sqp_14872e41e80b058a3017eb22bf67444aa6375f7d'''
             }
-            
         }
-        stage('Docker-build'){
+                stage('Docker-build'){
             steps{
+                withDockerRegistry(credentialsId: 'docker', url: 'https://index.docker.io/v1/')
+                {
                 sh '''
                     cd FlightReservationApplication
-                    docker build . -t mayurwagh/flightreservation-new:latest
-                    docker push mayurwagh/flightreservation-new:latest
-                    docker rmi mayurwagh/flightreservation-new:latest
+                    docker build -t abhi2578/flightreservation-new:latest .
+                    docker push abhi2578/flightreservation-new:latest
+                    docker rmi abhi2578/flightreservation-new:latest
                 '''
+               }
             }
         }
-        stage('Deploy'){
-            steps{
-                sh '''
-                    cd FlightReservationApplication
-                    kubectl apply -f k8s/
-                '''
-            }
-        }
+
     }
 }
